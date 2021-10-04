9f4f4972757f56d1a7b66791dae0fa741a532ada
Asif2990 <masif2019@namal.edu.pk>
2021-10-04 21:49:13 +0500

    Square root function

---
+++ cal05.py
@@ -0,0 +1,23 @@
+# calculator in Python(v0.3)
+
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2)
+	Return num1 - num2
+
+def mul(num1, num2)
+	return num1 * num2
+
+def Mod(num1,num2):
+	return num1 % num2
+def sqr(number):
+	return number ** 0.5
+# main
+number = int(input("Enter a number: "))
+res = add(5, 3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(Mod(4,2))
+prin("The square root of ", number, "is", sqr(number))
